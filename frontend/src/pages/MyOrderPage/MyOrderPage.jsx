76711ec6504991c5320ac554469c9ee9717efd97
Priyadas732 <pd2820523@gmail.com>
2025-07-11 23:02:04 +0530

    add

--- frontend/src/pages/MyOrderPage/MyOrderPage.jsx
+++ frontend/src/pages/MyOrderPage/MyOrderPage.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import MyOrder from '../../components/MyOrder/MyOrder'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 const MyOrderPage = () => {
+  useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
   return (
     <>
        <Navbar />
